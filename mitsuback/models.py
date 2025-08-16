8541f694e291030eea83491e04d63eb812ac4e0c
Anurag Sharma <anurag.sharma@webmobriltechnologies.com>
2025-08-16 18:56:45 +0530

    company_name

--- mitsuback/models.py
+++ mitsuback/models.py
@@ -12,9 +12,10 @@ class UserProfile(models.Model):
 
 class Register(models.Model):
     full_name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    # email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=100)
-    employee_id = models.CharField(max_length=100,unique=True)
+    company_name = models.CharField(max_length=100)
+    # employee_id = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- mitsuback/serializers.py
+++ mitsuback/serializers.py
@@ -5,11 +5,15 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Register
         fields = '__all__'
+    def validate(self, attrs):
+        if Register.objects.filter(phone_number=attrs['phone_number']).exists():
+            raise serializers.ValidationError("Phone number already exists")
+        return attrs
 
 class WinningListSerializer(serializers.ModelSerializer):
     user_name = serializers.CharField(source='user.full_name', read_only=True)
-    user_email = serializers.CharField(source='user.email', read_only=True)
+    user_company_name = serializers.CharField(source='user.company_name', read_only=True)
     
     class Meta:
         model = WinningList
-        fields = ['id', 'user', 'user_name', 'user_email', 'initial', 'won_at']
+        fields = ['id', 'user', 'user_name', 'user_company_name', 'initial', 'won_at']
